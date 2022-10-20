11e94cf72f516878339a0ffa0fb360cf91383cf3
jbecker <julix.beker@gmail.com>
2022-10-20 11:32:58 +0200

    [Lect. 01] Add Pound into Kilo Processor

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import input.GetFloatFromInputService;
-import tasks.RectangelCalculator;
+import tasks.PoundIntoKiloProcessor;
 
 
 public class Main {
     public static void main(String[] args) {
         //InputOutputFun.inputOutputFun();
         //PrintExampleLect01.printExample();
-        RectangelCalculator.calculate(
+        /*RectangelCalculator.calculate(
                 GetFloatFromInputService.getFloatFromUserWithMessage("Insert height"),
                 GetFloatFromInputService.getFloatFromUserWithMessage("Insert width"));
+        */
+        PoundIntoKiloProcessor.poundsIntoKilo(GetFloatFromInputService.getFloatFromUserWithMessage("Insert Pounds:"));
 
 
 
---
+++ src/tasks/PoundIntoKiloProcessor.java
@@ -0,0 +1,8 @@
+package tasks;
+
+public class PoundIntoKiloProcessor {
+    public static void poundsIntoKilo(float pounds) {
+        float kg = pounds / 2.2f;
+        System.out.println(pounds + " lb. = " + kg + " kg.");
+    }
+}
