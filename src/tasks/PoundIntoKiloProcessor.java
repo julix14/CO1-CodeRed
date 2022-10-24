7826d4c5b1f83fff3606527d5997b0fd651d848b
jbecker <julix.beker@gmail.com>
2022-10-24 17:10:43 +0200

    [HW-24-10-22] Add Task Interface and refactor Task Classes

--- src/tasks/PoundIntoKiloProcessor.java
+++ src/tasks/PoundIntoKiloProcessor.java
@@ -2,13 +2,13 @@ package tasks;
 
 import valdidation.ValidationService;
 
-public class PoundIntoKiloProcessor {
+public class PoundIntoKiloProcessor implements TaskInterface {
     public final String name = "Pound into Kilo Processor";
     private final ValidationService validationService = new ValidationService();
     public void act(){
         this.poundsIntoKilo(validationService.validateInputIsFloat("Insert Pounds:"));
     }
-    public void poundsIntoKilo(float pounds) {
+    private void poundsIntoKilo(float pounds) {
         float kg = pounds / 2.2f;
         System.out.println(pounds + " lb. = " + kg + " kg.");
     }
