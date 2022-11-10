286aca2e4d83150ccf10224cbe9296dc879942a1
jbecker <julix.beker@gmail.com>
2022-11-10 11:01:38 +0100

    [Lect. 04] Optimize RectangleCalculator

--- src/tasks/lecture1/RectangleCalculator.java
+++ src/tasks/lecture1/RectangleCalculator.java
@@ -14,10 +14,18 @@ public class RectangleCalculator implements TaskInterface {
                 validationService.validateInputIsFloat("Insert width"));
     }
     private void calculate(float height, float width) {
-        float area = height * width;
-        float perimeter = 2 * (height + width);
+        float area = calculateArea(height, width);
+        float perimeter = calculatePerimeter(height, width);
         System.out.println("Area: " + area);
         System.out.println("Perimeter: " + perimeter);
+    }
+
+    private float calculateArea(float height, float width) {
+        return height * width;
+    }
+
+    private float calculatePerimeter(float height, float width) {
+        return 2 * (height + width);
     }
 
     public String getName() {
