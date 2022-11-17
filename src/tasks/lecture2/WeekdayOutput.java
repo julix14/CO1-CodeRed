7f6eae239fd927c28568c2549d857af83dc4398b
jbecker <julix.beker@gmail.com>
2022-11-17 11:29:46 +0100

    [Lect. 05] Update WeekdayOutput

--- src/tasks/lecture2/WeekdayOutput.java
+++ src/tasks/lecture2/WeekdayOutput.java
@@ -16,24 +16,24 @@ public class WeekdayOutput implements TaskInterface {
     private void outputWeekday(int weekday, String language) {
         if (language.equalsIgnoreCase("en")) {
             switch (weekday) {
-                case 0 -> System.out.println("Monday");
-                case 1 -> System.out.println("Tuesday");
-                case 2 -> System.out.println("Wednesday");
-                case 3 -> System.out.println("Thursday");
-                case 4 -> System.out.println("Friday");
-                case 5 -> System.out.println("Saturday");
-                case 6 -> System.out.println("Sunday");
+                case 1 -> System.out.println("Monday");
+                case 2 -> System.out.println("Tuesday");
+                case 3 -> System.out.println("Wednesday");
+                case 4 -> System.out.println("Thursday");
+                case 5 -> System.out.println("Friday");
+                case 6 -> System.out.println("Saturday");
+                case 7 -> System.out.println("Sunday");
                 default -> System.out.println("Invalid weekday");
             }
         } else if (language.equalsIgnoreCase("de")) {
             switch (weekday) {
-                case 0 -> System.out.println("Montag");
-                case 1 -> System.out.println("Dienstag");
-                case 2 -> System.out.println("Mittwoch");
-                case 3 -> System.out.println("Donnerstag");
-                case 4 -> System.out.println("Freitag");
-                case 5 -> System.out.println("Samsatg");
-                case 6 -> System.out.println("Sonntag");
+                case 1 -> System.out.println("Montag");
+                case 2 -> System.out.println("Dienstag");
+                case 3 -> System.out.println("Mittwoch");
+                case 4 -> System.out.println("Donnerstag");
+                case 5 -> System.out.println("Freitag");
+                case 6 -> System.out.println("Samsatg");
+                case 7 -> System.out.println("Sonntag");
                 default -> System.out.println("Invalid weekday");
             }
         } else {
